ecb262a134a0950478b4a99d2bebdaeb9f79ac47
Sebastien Fleury <sebastien.fleury@epita.fr>
2019-02-25 16:15:51 +0100

    unique return type: JsObject

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -1,7 +1,7 @@
 import java.io.File
 
 import com.github.tototoshi.csv.CSVReader
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsObject, JsString, JsValue, Json}
 
 import scala.io.Source
 
@@ -12,18 +12,20 @@ object DataLoader {
     else
       List(file)
 
-  def loadJsonData(file: File): List[JsValue] = Source.fromFile(file)
+  def loadJsonData(file: File): List[JsObject] = Source.fromFile(file)
     .getLines() // Each file is not strictly an array of Json, it contains a Json Value per lines.
     .map(Json.parse)
+    .map(_.as[JsObject])
     .toList
 
-  def loadCsvData(file: File): List[Map[String, String]] = CSVReader.open(file)
+  def loadCsvData(file: File): List[JsObject] = CSVReader.open(file)
     .allWithHeaders()
+    .map(x => Json.toJsObject(x))
 
-  def loadData(files: List[File]): List[Either[JsValue, Map[String, String]]] = files
+  def loadData(files: List[File]): List[JsObject] = files
     .flatMap(file =>
-      if (file.getName.endsWith(".json")) loadJsonData(file).map(Left(_))
-      else if (file.getName.endsWith(".csv")) loadCsvData(file).map(Right(_))
+      if (file.getName.endsWith(".json")) loadJsonData(file)
+      else if (file.getName.endsWith(".csv")) loadCsvData(file)
       else Nil
     )
 }
