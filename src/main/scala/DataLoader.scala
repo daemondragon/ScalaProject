27cc259913c3974cb440b127bb1359adeac71a47
jeremy beuvry <jeremy.beuvry@epita.fr>
2019-02-28 12:00:41 +0100

    add Iterator for every DataLoader functions instead of List

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -1,28 +1,36 @@
 import java.io.File
 
 import com.github.tototoshi.csv.CSVReader
-import play.api.libs.json.{JsObject, JsString, JsValue, Json}
+import play.api.libs.json.{JsObject, Json}
 
 import scala.io.Source
 
+/**
+  * Every function here use iterator to allow lazy-loading:
+  * The IO are only performed when needed.
+  *
+  * Doing so allow to send all loaded data by pieces, preventing
+  * the need to load every files into memory before sending them to the server.
+  */
 object DataLoader {
-  def listFiles(file: File): List[File] =
-    if (file.isDirectory) //List all files recursively
-      file :: file.listFiles().flatMap(listFiles).toList
-    else
-      List(file)
 
-  def loadJsonData(file: File): List[JsObject] = Source.fromFile(file)
+  def listFiles(file: File): Iterator[File] =
+    if (file.isDirectory) //List all files recursively
+      Iterator(file) ++ file.listFiles().flatMap(listFiles).toIterator
+    else
+      Iterator(file)
+
+  def loadJsonData(file: File): Iterator[JsObject] = Source.fromFile(file)
     .getLines() // Each file is not strictly an array of Json, it contains a Json Value per lines.
     .map(Json.parse)
     .map(_.as[JsObject])
-    .toList
 
-  def loadCsvData(file: File): List[JsObject] = CSVReader.open(file)
+  def loadCsvData(file: File): Iterator[JsObject] = CSVReader.open(file)
     .allWithHeaders()
     .map(x => Json.toJsObject(x))
+    .toIterator
 
-  def loadData(files: List[File]): List[JsObject] = files
+  def loadData(files: Iterator[File]): Iterator[JsObject] = files
     .flatMap(file =>
       if (file.getName.endsWith(".json")) loadJsonData(file)
       else if (file.getName.endsWith(".csv")) loadCsvData(file)
