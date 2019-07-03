327df4dfb2bb8483e8e01e0914b18294216e4ffc
Mathieu Pochon <mathieu.pochon@epita.fr>
2019-07-03 22:48:10 +0200

    removed dirty code

--- ScalaSpark/src/main/scala/reader.scala
+++ ScalaSpark/src/main/scala/reader.scala
@@ -42,33 +42,6 @@ object reader {
       .filter(_.battery > 15)
       .count()
 
-    /*  var test = 0
-    for (str <- value) {
-      val g = new Gson()
-
-      val drone = g.fromJson(str, classOf[DroneData])
-      if (drone.defect == 1) {
-        if (drone.latitude >= 0) {
-          north = north + 1
-        } else {
-          south = south + 1
-        }
-        if (drone.temperature >= 20.0) {
-          hot = hot + 1
-        } else {
-          called = called + 1
-        }
-        if (drone.battery < 15.0) {
-          low = low + 1
-        } else {
-          notlow = notlow + 1
-        }
-      }
-      test = test + 1
-      println(test)
-    }
-    println(test)
-*/
     println("Proportion of failing devices in north hemisphere : " + ((100.0 * north / (north+south))).toString() + "%")
 
     if (hot > called) {
