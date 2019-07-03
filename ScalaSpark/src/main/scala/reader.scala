ed83fde58b90bc64e64741a60a469329a324d19e
Mathieu Pochon <mathieu.pochon@epita.fr>
2019-07-03 15:15:30 +0200

    added comptue fonction to compute stats on hdfs

--- ScalaSpark/src/main/scala/reader.scala
+++ ScalaSpark/src/main/scala/reader.scala
@@ -1,3 +1,4 @@
+import com.google.gson.Gson
 import org.apache.spark.sql.SparkSession
 
 object reader {
@@ -10,7 +11,21 @@ object reader {
 
     val f = Spark.sparkContext.textFile("./data.txt")
 
-
-    f.foreach(str => println(str))
+    f.foreach(str => compute(str))
   }
+
+  def compute(str: String) = {
+    val g = new Gson()
+
+    val drone = g.fromJson(str, classOf[DroneData])
+    if (drone.defect == 1) {
+      println(str)
+    }
+  }
+
+  /*
+  In proportion to the whole number of devices is there more failing devices in the north hemisphere or the south hemisphere?
+  Is there more failing devices when the weather is hot or when the weather is called?
+  Among the failing devices which percentage fails beceause of low battery or empty fuel tank?
+   */
 }
